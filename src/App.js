b5d5655f8be6f16cbd4c6dd03580819b85251722
Ahmed_Qannan <ahmedgithup@gmail.com>
2023-07-10 23:32:16 +0300

    Create file App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import HomePages from './pages/HomePages';
+// import CountersPage from './pages/Counters/Counters';
+import Counter2 from './pages/Counter2';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     {/* <HomePages /> */}
+     {/* <CountersPage /> */}
+     <Counter2 />
     </div>
   );
 }
